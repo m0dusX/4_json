df2695f442daa280ba55e40ae87ffd0e3102b952
m0dusX <31633375+m0dusX@users.noreply.github.com>
2017-09-12 17:14:43 +0300

    Написан основной код программы
    В задании было сказано "написать скрипт, который на вход принимает путь до файла..." поэтому в моем решении используется модуль sys для получения входных параметров.

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,15 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
-
+    with open(filepath, "r") as f:
+        return json.load(f)
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True))
+       
 
 
 if __name__ == '__main__':
-    pass
+    pretty_print_json(load_data(sys.argv[1]))
