c758fb4cbf4f83806e602364d9e0c1a6d73db56d
m0dusX <31633375+m0dusX@users.noreply.github.com>
2017-09-12 17:39:04 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -9,5 +9,5 @@ def load_data(filepath):
 def pretty_print_json(data):
     print(json.dumps(data, indent=4, sort_keys=True))
        
-if __name__ == '__main__':
+if __name__ == "__main__":
     pretty_print_json(load_data(sys.argv[1]))
