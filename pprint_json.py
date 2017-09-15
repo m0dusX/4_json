6b06ade647593bc9d0a9c54b80e0c35e98bd5ab2
m0dusX <modumoduso@gmail.com>
2017-09-15 12:47:18 +0300

    Изменен код

--- pprint_json.py
+++ pprint_json.py
@@ -3,12 +3,11 @@ import sys
 
 
 def load_data(filepath):
-    file_with_json = open(filepath, "a")
-    parsed_json = json.load(file_with_json)
-    return parsed_json
+	with open (filepath, 'r', encoding="utf-8") as file:
+		return json.load(file)
 
 def pretty_print_json(data_to_print):
-    print(json.dumps(data_to_print, indent=4, sort_keys=True))
+    print(json.dumps(data_to_print, indent=4, sort_keys=True, ensure_ascii=False))
        
 if __name__ == "__main__":
     pretty_print_json(load_data(sys.argv[1]))
